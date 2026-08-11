db185308e931b3c2b0d6c68a61b6adb782d8b1e3
AtlanticMagud <dana.radonjic.88@gmail.com>
2026-08-11 23:39:45 +0200

    Add files via upload

--- app/sw.js
+++ app/sw.js
@@ -4,27 +4,27 @@
 // Uloga: aplikacija mora raditi i bez mreže (u radnji signal zna da padne), a nova
 // objava na GitHub Pages-u mora sama stići do telefona. Zato se pri instalaciji
 // keširaju svi fajlovi verzije, a stari keš se briše pri aktivaciji.
-const VERZIJA = "1.0.22"
+const VERZIJA = "1.0.24"
 const KES = 'knjigovodstvo-' + VERZIJA
 const FAJLOVI = [
-  "assets/PinDijalog-FbCX1LLQ.js",
-  "assets/artikli-DGkxov8-.js",
-  "assets/bankpdf-Byllzths.js",
-  "assets/cijene-BzGC9yJw.js",
+  "assets/PinDijalog-CtS6WSgZ.js",
+  "assets/artikli-DKUcEFaS.js",
+  "assets/bankpdf-BLXdqemp.js",
+  "assets/cijene-DKak7xK5.js",
+  "assets/index-BC882QA8.js",
   "assets/index-BTn7_ZV6.css",
+  "assets/index-CMuzVTKY.js",
+  "assets/index-CyhYX12c.js",
   "assets/index-DNzFMgCo.js",
-  "assets/index-DVUKRq3J.js",
-  "assets/index-DgxSq92f.js",
-  "assets/index-J40I4qVO.js",
-  "assets/index-ZXDl0I5T.js",
-  "assets/index-dBHz65c2.js",
+  "assets/index-JHiktRjH.js",
+  "assets/index-MaOEXdP6.js",
   "assets/index-ggsP5-uy.js",
   "assets/pdf.worker-Mx0w3D2U.js",
   "assets/pdftekst-FlZV6gvs.js",
-  "assets/renumeracija-AANDuQ8w.js",
+  "assets/renumeracija-CZvkjrZo.js",
   "assets/sql-wasm-UFUCzYNW.wasm",
-  "assets/web-BWOt1AaE.js",
   "assets/web-BXJjiGQ6.js",
+  "assets/web-CK93GYmt.js",
   "assets/web-DRnNXkco.js",
   "assets/web-sfuwvzF6.js",
   "ikone/ikona-180.png",
